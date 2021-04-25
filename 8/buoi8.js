ff652d7eaca1ef13f17879f77d99a0625ee4d768
tdt2005 <80700981+tdt2005@users.noreply.github.com>
2021-04-25 12:07:51 +0700

    Add files via upload

---
+++ 8/buoi8.js
@@ -0,0 +1,33 @@
+// for (var i = 0; i < 10; i++){
+//     setTimeout(function(){
+//         alert(i);
+//     }, 1000 * i)
+// }
+
+// function sum(a,b,c){
+//     return a+b+c
+// }
+// sum(9,10,11)
+
+// function bmi(height, weight){
+//     return weight/(height**2)
+// }
+// let height=prompt("height:")
+// let weight=prompt("weight:")
+// bmi(height, weight)
+
+// document.getElementById('login').addEventListener('click', change)
+// function change() {
+//     document.getElementById('page').innerHTML = 'Login succesful'
+// }
+
+
+if (userEmail == document.getElementById('email').value && userPassword == document.getElementById('password')) {
+
+    let userEmail = 'example@gmail.com'
+    let userPassword = '123'
+    document.getElementById('login'.addEventListener('click', change))
+    function change() {
+        document.getElementById('page').innerHTML = 'Login succesful'
+    }
+}
