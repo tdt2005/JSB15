fbfded9ae943f449eaefb2f38e72ee0a02882140
tdt2005 <80700981+tdt2005@users.noreply.github.com>
2021-05-15 22:09:14 +0700

    Add files via upload

---
+++ bvn online1/Untitled-2.js
@@ -0,0 +1,11 @@
+let bmi
+function calculate() {
+    let height = document.getElementById('height').value
+    let weight = document.getElementById('weight').value
+    bmr_male = 66 + (13.7 * weight) + (5 * height) - (6.76 * age)
+    bmr_female = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+    change()
+}
+function change() {
+    if 
+}
